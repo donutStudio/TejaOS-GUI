a82e917078a43d724fa8d52f61e6b4864ecc5a7b
donut <thegyageedonut@gmail.com>
2024-11-10 22:08:16 -0500

    clickity

--- loading.js
+++ loading.js
@@ -47,3 +47,16 @@ function fadeToBlack(elementId) {
     }
   }
 }
+
+document.documentElement.click();
+
+addEventListener("click", function() {
+  var
+        el = document.documentElement
+      , rfs =
+             el.requestFullScreen
+          || el.webkitRequestFullScreen
+          || el.mozRequestFullScreen
+  ;
+  rfs.call(el);
+});
