f361a66c0809a8f60685b75831293415e5ee165c
donut <thegyageedonut@gmail.com>
2024-11-13 00:45:14 -0500

    sdf

--- reset.js
+++ reset.js
@@ -1,4 +1,4 @@
-window.parent.document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, UIScrollViewContentInsetAdjustmentNever, minimumscale=0.25, maximumscale=5, height=1024, user-scalable=yes, initial-scale=1, viewport-fit=cover");
+window.parent//.document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, UIScrollViewContentInsetAdjustmentNever, minimumscale=0.25, maximumscale=5, height=1024, user-scalable=yes, initial-scale=1, viewport-fit=cover");
 
 document.addEventListener('keydown', function(event) {
     // Check if the focused element is a text input, textarea, or any editable field
